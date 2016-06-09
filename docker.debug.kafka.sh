c441f8e8ae2c6981852e50b123479f9d5838701d
Damien <dgarros@gmail.com>
2016-06-08 23:34:19 -0700

    Cleanup
    Remove unused plugins
    Remove config not used
    etc ..

--- docker.debug.kafka.sh
+++
@@ -1,8 +0,0 @@
-
-docker run -d -p 2181:2181 \
-            -p 9092:9092 \
-            --env ADVERTISED_PORT=9092 \
-            --env ADVERTISED_HOST="172.29.110.168" \
-            spotify/kafka
-
-          #  --env ADVERTISED_HOST= \
